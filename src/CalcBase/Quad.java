69ecd40f538f43ffcdc8751da20311d3a34b0c15
Silv3rh4t <akhandyadav.com@gmail.com>
2017-01-24 14:48:04 +0530

    Now Master is the Only branch
    I don't know how to discard older branches

--- src/CalcBase/Quad.java
+++ src/CalcBase/Quad.java
@@ -1,23 +1,22 @@
 package CalcBase;
 
 import ShortHand.*;
-import java.io.*;
 public class Quad
 {
-    public static void main()throws IOException
+    public static void main()
     {
-        u.pln("Enter Terms as per their location in the following Equation");
-        u.pln("Equation -> ax(square) [+/-] bx [+/-] c");
-        double a=u.iSd("Enter a");
-        double b=u.iSd("Enter b");
-        double c=u.iSd("Enter c");
-        char x=u.iBc("Enter x[variable used]");
+
+        u.pln("Enter Equation [use only 'x' as the variable]");
+        u.pln("E.g.-> 2x^2[+/-]4x[+/-]1");
+    	String eq=u.iSs();
+    	int a=Integer.parseInt(eq.substring(0,eq.indexOf('x')));
+    	
         double d= (b*b)-(4*a*c);
-        double r1= ((b-(2*b)) + Math.pow(d,0.5))/(2*a);
-        double r2= ((b-(2*b)) - Math.pow(d,0.5))/(2*a);
-        u.pln(x+"Root 1 => "+r1);
+        double r1= ((-b) + Math.pow(d,0.5))/(2*a);
+        double r2= ((-b) - Math.pow(d,0.5))/(2*a);
+        u.pln("Root 1 => "+r1);
         Decimal.utjc(r1,"Root 1");
-        u.pln(x+"Root 2 => "+r2);
+        u.pln("Root 2 => "+r2);
         Decimal.utjc(r2,"Root 2");
         u.pln("Determinant => "+d);
         if (d>0)u.pln("Virtual Roots");
